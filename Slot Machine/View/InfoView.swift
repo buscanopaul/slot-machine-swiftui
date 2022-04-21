b26c3939da04af607fdbfb2fe30af140d93b82f9
Paul Buscano <pbuscano@deloitte.com>
2022-04-21 11:10:59 +0800

    First commit

---
+++ Slot Machine/View/InfoView.swift
@@ -0,0 +1,70 @@
+//
+//  InfoView.swift
+//  Slot Machine
+//
+//  Created by pbuscano on 4/20/22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    // MARK: - PROPERTIES
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            LogoView()
+            
+            Spacer()
+            
+            Form {
+                Section(header: Text("About the application")) {
+                    FormRowView(firstItem: "Application", secondItem: "Slot Machine")
+                    FormRowView(firstItem: "Platforms", secondItem: "iPhone, Ipad, Mac")
+                    FormRowView(firstItem: "Developer", secondItem: "Paul Buscano")
+                    FormRowView(firstItem: "Designer", secondItem: "Robert Petras")
+                    FormRowView(firstItem: "Website", secondItem: "buscanopaul.com")
+                } //: SECTION
+            } //: FORM
+            .font(.system(.body, design: .rounded))
+        }
+        .padding(.top, 40)
+        .overlay(
+            Button(action: {
+                audioPlayer?.stop()
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "xmark.circle")
+                    .font(.title)
+            }
+            .padding(.top, 30)
+            .padding(.trailing, 20)
+            .accentColor(Color.secondary)
+            , alignment: .topTrailing
+        )
+        .onAppear(perform: {
+            playSound(sound: "background-music", type: "mp3")
+        })
+    }
+}
+
+struct FormRowView: View {
+    var firstItem : String
+    var secondItem: String
+    
+    var body: some View {
+        HStack {
+            Text(firstItem).foregroundColor(Color.gray)
+            Spacer()
+            Text(secondItem)
+        }
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
+
+
